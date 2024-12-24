29749958d23afbd264a23cbf188618a26200759b
Sky Blue <dawadiaakash@gmail.com>
2024-12-24 11:18:42 +0545

    fix: user registeration and login

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 def home(request):
@@ -35,3 +39,26 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            # Authenticate the user using their credentials
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have registered successfully. Welcome!")
+                return redirect('home')
+            else:
+                messages.error(request, "There was a problem logging you in. Please try again.")
+        else:
+            messages.error(request, "Oops, there was a problem. Try again.")
+            return redirect('register')
+    return render(request, 'register.html', {'form': form})
