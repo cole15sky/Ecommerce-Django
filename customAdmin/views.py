10bd2278f77dc33a32c6d4f64edede943c9c54e8
Sky Blue <dawadiaakash@gmail.com>
2025-01-14 15:46:21 +0545

    feat: initialize models and views for dashboard page

--- customAdmin/views.py
+++ customAdmin/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login
+from store.models import Category, Customer, Product, Order 
 
 def dashboard(request):
-    return render(request, 'customAdmin/dashboard.html', {})
+    context = {
+        'categories': Category.objects.all(),
+        'customers': Customer.objects.all(),
+        'products': Product.objects.all(),
+        'orders': Order.objects.all(),
+    }
+    return render(request, 'customAdmin/dashboard.html', context)  
