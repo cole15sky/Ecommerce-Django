c4c663b556b2a81422d3678a9bb8e66ae76e9e22
whywhy <84124699+loshanparajuli@users.noreply.github.com>
2025-02-21 18:26:38 +0545

    Redundancy removed from cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -3,30 +3,14 @@ from store.models import Product
 class Cart:
     def __init__(self, request):
         self.session = request.session
-
-        # Get or initialize the session_key for the cart
-        cart = self.session.get('session_key')
-        if 'session_key' not in self.session:
-            cart = self.session['session_key'] = {}
-        self.cart = cart
+        self.cart = self.session.setdefault('session_key', {})
 
     def add(self, product):
-        product_id = str(product.id)
-
-        if product_id not in self.cart:
-            self.cart[product_id] = {'price': str(product.price)}
-
+        self.cart.setdefault(str(product.id), {'price': str(product.price)})
         self.session.modified = True
-        
-        
-        
+
     def __len__(self):
-         return len(self.cart)
-     
-     
+        return len(self.cart)
+
     def get_prdt(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in = product_ids)
-        
-        return products
-        
+        return Product.objects.filter(id__in=self.cart.keys())
